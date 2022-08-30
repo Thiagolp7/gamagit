f6bf1c2eb9d7e8a5edce70bfe5684eb672edddf6
Thiago Paixão <thiago.paixao@enext.com.br>
2022-08-30 08:15:06 -0300

    feat: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
-import { SearchBar } from "../../components/SearchBar";
 import { GamaLogo } from "../../components/Logo/GamaLogo";
-import { Container } from "./styles";
+import { SearchBar } from "../../components/SearchBar";
 import { SearchResult } from "../../components/SearchResults";
 import { Loading } from "../../components/Loading";
 import { useLoading } from "../../hooks/useLoading";
+import { Container } from "./styles";
 import { useNavigate } from "react-router-dom";
 
 
